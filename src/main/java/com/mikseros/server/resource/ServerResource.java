cbb18a8fdec83814aadd64befb109e9c6f7b2706
mikseros <mmadynski@gmail.com>
2022-05-15 19:45:39 +0200

    Server Controller

---
+++ src/main/java/com/mikseros/server/resource/ServerResource.java
@@ -0,0 +1,35 @@
+package com.mikseros.server.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mikseros.server.model.Response;
+import com.mikseros.server.service.implementation.ServerServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+import java.util.Map;
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
+
+// Technically it is controller
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+	private final ServerServiceImpl serverService;
+	
+	@GetMapping("/list")
+	public ResponseEntity<Response> getServers() {
+		return ResponseEntity.ok(
+				Response.builder()
+						.timeStamp(now())
+						.data(Map.of("servers", serverService.list(30)))
+						.message("Servers retrieved")
+						.status(OK)
+						.statusCode(OK.value())
+						.build()
+		);
+	}
+}
